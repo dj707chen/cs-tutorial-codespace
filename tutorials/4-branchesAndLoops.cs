9ecef039f0fc1c72a01378c3df897e7a4527f558
DJ Chen <80285637+dj707chen@users.noreply.github.com>
2026-09-13 15:53:14 -0500

    Save

--- tutorials/4-branchesAndLoops.cs
+++ tutorials/4-branchesAndLoops.cs
@@ -17,8 +17,31 @@ else
 // Use loops to repeat operations
 // https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/tutorials/branches-and-loops#use-loops-to-repeat-operations
 int counter = 0;
-while (counter < 10)
+Console.WriteLine();
+while (counter < 6)
 {
     Console.WriteLine($"Hello World! The counter is {counter}");
     counter++;
 }
+
+// https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/tutorials/branches-and-loops#created-nested-loops
+Console.WriteLine();
+for (int row = 1; row < 5; row++)
+{
+    for (char column = 'a'; column < 'k'; column++)
+    {
+        Console.WriteLine($"The cell is ({row}, {column})");
+    }
+}
+
+// https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/tutorials/branches-and-loops#combine-branches-and-loops
+int sum = 0;
+Console.WriteLine();
+for (int number = 1; number < 21; number++)
+{
+    if (number % 3 == 0)
+    {
+        sum = sum + number;
+    }
+}
+Console.WriteLine($"The sum is {sum}");
