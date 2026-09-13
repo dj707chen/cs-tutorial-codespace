0394ce82ce0b37f31ad39a00a953169368eceb6b
DJ Chen <80285637+dj707chen@users.noreply.github.com>
2026-09-12 20:50:58 -0500

    Save hello-world.cs playing with strings

--- tutorials/hello-world.cs
+++ tutorials/hello-world.cs
@@ -1,1 +1,42 @@
+// https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/tutorials/hello-world
 Console.WriteLine("Hello, World!");
+
+string aFriend = "Bill";
+Console.WriteLine(aFriend);
+
+aFriend = "Kellie";
+Console.WriteLine(aFriend);
+
+Console.WriteLine("Hello " + aFriend);
+Console.WriteLine($"Hello {aFriend}");
+
+string firstFriend = "Maria";
+string secondFriend = "Sage";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+
+// Remove whitespace from strings
+// https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/tutorials/hello-world#remove-whitespace-from-strings
+string greeting = "      Hello World!       ";
+Console.WriteLine();
+Console.WriteLine($"[{greeting}]");
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+string sayHello = "Hello World!";
+Console.WriteLine();
+Console.WriteLine(sayHello);
+sayHello = sayHello.Replace("Hello", "Greetings");
+Console.WriteLine(sayHello);
+
+Console.WriteLine(sayHello.ToUpper());
+Console.WriteLine(sayHello.ToLower());
